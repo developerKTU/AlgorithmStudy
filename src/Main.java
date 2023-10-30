bd05a5a5290e0199c0461dff8f7bd2e5675f05e0
KTU <rlaxodnr457@naver.com>
2023-10-30 23:48:53 +0900

    Github Commit Test2 !

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         // 알고리즘 공부를 위한 자바 패키지
         System.out.println("Let's go Algorithm Study!");
+        System.out.println("Github Test!");
     }
 }
