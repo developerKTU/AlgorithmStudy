ba8a2ed712f164739e0bc8f7d7b434f850f4239b
KTU <rlaxodnr457@naver.com>
2023-11-20 14:02:09 +0900

    Chapter03 -> Stack and Queue Scanner, print Modify Commit 23/11/20

--- src/Chapter03/ArrayAndList/question/ArrayAndList02.java
+++ src/Chapter03/ArrayAndList/question/ArrayAndList02.java
@@ -1,29 +1,37 @@
 package Chapter03.ArrayAndList.question;
 
+import java.io.*;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
+/* 백준 - 1546번 문제 */
 public class ArrayAndList02 {
-    public static void main(String [] args){
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new BufferedReader(new InputStreamReader(System.in)));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        /* 과목 수 입력 */
+        int N = Integer.parseInt(br.readLine());
         int[] scoreArr = new int[N];
         int M = 0;
         int tot = 0;
 
+        /* 점수 입력 받기. */
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for(int i = 0; i < N; i++){
-            scoreArr[i] = sc.nextInt();
+            scoreArr[i] = Integer.parseInt(st.nextToken());
             tot += scoreArr[i];
         }
 
-        sc.close();
-
+        /* 최댓값 M 구하기 */
         for(int i : scoreArr){
             if(i > M){
                 M = i;
             }
         }
 
-        System.out.println( (double)(tot * 100)/M/N );
+        bw.write(String.valueOf((double)(tot * 100)/M/N));
+        bw.close();
 
     }
 }
