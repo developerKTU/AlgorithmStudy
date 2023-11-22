5bbca2cf78725c9db02a045799da71d3cd0604b8
KTU <rlaxodnr457@naver.com>
2023-11-22 17:17:07 +0900

    Chapter03 -> PrefixSumExample modify Commit 23/11/22

--- src/Chapter03/PrefixSum/example/PrefixSumExample.java
+++ src/Chapter03/PrefixSum/example/PrefixSumExample.java
@@ -8,14 +8,14 @@ public class PrefixSumExample {
         int result = 0;
 
         // 합배열 S 구하기
-        int[] S = new int[A.length+1];
-        S[0] = 0;
+        int[] S = new int[A.length];
+        S[0] = A[0];
 
-        for(int i = 1; i < A.length+1; i++){
-            S[i] = S[i-1] + A[i-1];
+        for(int i = 1; i < A.length; i++){
+            S[i] = S[i-1] + A[i];
         }
 
-        // A배열의 첫번째 부터 세번째 까지 합 --> 29
+        // A[1]부터 A[3] 까지 합 --> 47
         result = S[3] - S[0];  // S[j] - S[i-1] --> S[3] - s[1-1]
 
         System.out.println(result);
