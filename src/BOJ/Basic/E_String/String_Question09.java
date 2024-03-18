f72ea8f536e7aa68cc98e2e43a25695f46eefe56
KTU <rlaxodnr457@naver.com>
2024-03-18 19:36:31 +0900

    Baekjoon Basic Study Commit : 24/03/18

--- src/BOJ/Basic/E_String/String_Question09.java
+++ src/BOJ/Basic/E_String/String_Question09.java
@@ -4,6 +4,7 @@ import java.io.*;
 
 public class String_Question09 {
     public static void main(String[] args) throws IOException{
+        /* 문제번호 25083 */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
---
+++ src/BOJ/Deep/DeepQuestion01.java
@@ -0,0 +1,21 @@
+package BOJ.Deep;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+public class DeepQuestion01 {
+    public static void main(String[] args) throws IOException {
+        /* 문제번호 25083 */
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        bw.write("         ,r'\"7"+"\n");
+        bw.write("r`-_   ,'  ,/"+"\n");
+        bw.write(" \\. \". L_r'"+"\n");
+        bw.write("   `~\\/"+"\n");
+        bw.write("      |"+"\n");
+        bw.write("      |"+"\n");
+        bw.close();
+    }
+
+}
