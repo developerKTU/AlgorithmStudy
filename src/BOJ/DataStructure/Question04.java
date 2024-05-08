6b982d876f1ec28b720bb40ebdfbe9e8d61cf4c8
KTU <rlaxodnr457@naver.com>
2024-05-08 13:41:52 +0900

    Algorithm study Commit (if -> switch)

--- src/BOJ/DataStructure/Question04.java
+++ src/BOJ/DataStructure/Question04.java
@@ -11,47 +11,54 @@ public class Question04 {
 
         int T = Integer.parseInt(br.readLine());
 
-        Queue<Integer> queue = new LinkedList<>();
-
         int n = 0;
+        Queue<Integer> queue = new LinkedList<>();
         for(int i = 0; i < T; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             String commend = st.nextToken();
 
-            if("push".equals(commend)){
-                n = Integer.parseInt(st.nextToken());
-                queue.add(n);
-            }else if("pop".equals(commend)){
-                if(queue.isEmpty()){
-                    System.out.println(-1);
-                }else{
-                    int popNum = queue.poll();
-                    System.out.println(popNum);
-                }
-            }else if("size".equals(commend)){
-                System.out.println(queue.size());
-            }else if("empty".equals(commend)){
-                if(queue.isEmpty()){
-                    System.out.println(1);
-                }else{
-                    System.out.println(0);
-                }
-            }else if("front".equals(commend)){
-                if(queue.isEmpty()){
-                    System.out.println(-1);
-                }else{
-                    System.out.println(queue.peek());
-                }
-            }else if("back".equals(commend)){
-                if(queue.isEmpty()){
-                    System.out.println(-1);
-                }else{
-                    System.out.println(n);
-                }
+            switch(commend){
+                case "push":
+                    n = Integer.parseInt(st.nextToken());
+                    queue.add(n);
+                    break;
+                case "pop":
+                    if(queue.isEmpty()){
+                        System.out.println(-1);
+                        break;
+                    }else{
+                        int popNum = queue.poll();
+                        System.out.println(popNum);
+                        break;
+                    }
+                case "size":
+                    System.out.println(queue.size());
+                    break;
+                case "empty":
+                    if(queue.isEmpty()){
+                        System.out.println(1);
+                        break;
+                    }else{
+                        System.out.println(0);
+                        break;
+                    }
+                case "front":
+                    if(queue.isEmpty()){
+                        System.out.println(-1);
+                        break;
+                    }else{
+                        System.out.println(queue.peek());
+                        break;
+                    }
+                case "back":
+                    if(queue.isEmpty()){
+                        System.out.println(-1);
+                        break;
+                    }else{
+                        System.out.println(n);
+                        break;
+                    }
             }
-
         }
-
     }
 }
