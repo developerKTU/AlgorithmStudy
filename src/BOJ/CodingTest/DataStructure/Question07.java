3adb5f41dc6b5e46b0710acbe19aa5f9cac1fe29
dnrdldnrl <rlaxodnr457@naver.com>
2024-05-22 00:28:04 +0900

    잘못 생성된 클래스 삭제

--- src/BOJ/CodingTest/DataStructure/Question07.java
+++
@@ -1,9 +0,0 @@
-package BOJ.CodingTest.DataStructure;
-
-import java.io.*;
-
-public class Question07 {
-    public static void main(String[] args) throws IOException {
-        /* 백준 9372번 문제 https://www.acmicpc.net/problem/9372 */
-    }
-}
