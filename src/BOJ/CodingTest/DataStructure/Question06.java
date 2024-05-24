e7abc796b242f38eab331dd9468fd69e47bb5b40
KTU <rlaxodnr457@naver.com>
2024-05-24 17:52:33 +0900

    Algorithm study Commit - 문자열 문제 주석수정

--- src/BOJ/CodingTest/DataStructure/Question06.java
+++ src/BOJ/CodingTest/DataStructure/Question06.java
@@ -15,6 +15,9 @@ public class Question06 {
         int result = 50;
 
         for(int i = 0; i < B.length() - A.length() + 1; i++){   // A 문자열 길이만큼 B문자열과 비교해야함.
+            /* 각 케이스마다 최소값을 구해야함
+               ex) ABCD ARGCEF 비교시 ABCD <-> ARGC, ABCD <-> RGCE, ABCD <-> GCEF 비교하여
+               각 케이스 중 가장 작은 값(A, B 차이 최소) 추출 */
             int cnt = 0;
             for(int j = 0; j < A.length(); j++){                // A 문자열과 B문자열 비교 (A 문자열과 B 문자열의 차이 만큼 옆으로 이동하면서 비교)
                 if(A.charAt(j) != B.charAt(i + j)){             // A 문자열의 문자와 B문자열의 문자가 다르면 cnt++
