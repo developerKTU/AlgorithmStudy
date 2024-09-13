23efd93c8ad83ef62f011721b640d59dc0ac4467
dnrdldnrl <rlaxodnr457@naver.com>
2024-09-13 23:01:47 +0900

    Algorithm Study - 백준 : 11659 구간 합 문제 풀이 및 알고리즘 일정표 업데이트

--- src/BOJ/CodingTest/DataStructure/PrefixSum/Question01.java
+++ src/BOJ/CodingTest/DataStructure/PrefixSum/Question01.java
@@ -4,8 +4,34 @@ import java.io.*;
 import java.util.*;
 
 public class Question01 {
-    public static void main(String[] args) throws IOException{
-        /* 백준 11659번 문제 https://www.acmicpc.net/problem/11659 */
+    public static int[] twoPointer(int[] A, int[][] command){
+
+        int[] copyA = new int[A.length + 1];
+        int[] answer = new int[command.length];
+
+        copyA[0] = 0;
+        for(int i = 1; i < copyA.length; i++){
+            copyA[i] = A[i - 1];
+        }
+
+        // 합배열 S 구하기
+        int[] S = new int[copyA.length];
+        S[0] = copyA[0];
+        for(int i = 1; i < copyA.length; i++){
+            S[i] = S[i - 1] + copyA[i];
+        }
+
+        // 구간합 구하기
+        for(int t = 0; t < command.length; t++){
+            int i = command[t][0];
+            int j = command[t][1];
+
+            answer[t] = S[j] - S[i - 1];
+        }
+
+        return answer;
+    }
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
@@ -14,32 +40,26 @@ public class Question01 {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        // 주어진 수를 담을 배열 A선언
-        int[] A = new int[N + 1];
-        // 합 배열을 저장할 배열 S 선언
-        int[] S = new int[A.length];
+        int[] arr = new int[N];
 
-        // 배열 A 초기화
         st = new StringTokenizer(br.readLine());
-        A[0] = 0;
-        for(int i = 1; i < A.length; i++){
-            A[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 합 배열 S 초기화
-        S[0] = A[0];
-        for(int i = 1; i < A.length; i++){
-            S[i] = S[i - 1] + A[i];
-        }
+        int[][] command = new int[M][2];
 
-        for(int t = 0; t < M; t++){
+        for(int x = 0; x < command.length; x++){
             st = new StringTokenizer(br.readLine());
-            // 인덱스는 0부터 시작하지만 구간 합은 시작이 1부터 시작이라 -1 처리하지 않음.
-            int i = Integer.parseInt(st.nextToken());
-            int j = Integer.parseInt(st.nextToken());
-
-            System.out.println(S[j] - S[i - 1]);
+            for(int y = 0; y < command[y].length; y++){
+                command[x][y] = Integer.parseInt(st.nextToken());
+            }
         }
 
+        int[] result = twoPointer(arr, command);
+
+        for (int i : result) {
+            System.out.println(i);
+        }
     }
 }
